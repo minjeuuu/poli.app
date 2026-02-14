f703873296dc2b41a1e625821a6fd5f7475124e4
minjeuuu <corpuzmatthew0814@gmail.com>
2026-02-15 01:54:32 +0800

    Update vite.config.ts

--- poli-ultimate-enhanced/vite.config.ts
+++ poli-ultimate-enhanced/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   },
   build: {
     target: 'esnext',
-    minify: 'terser',
+    minify: 'esbuild',
     sourcemap: false,
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
